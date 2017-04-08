64e565ee71d148ca3fe9d7cd34b66047914e2b55
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-08 16:26:40 -0300

    Added new images

--- app/controllers/api/v1/counters_controller.rb
+++ app/controllers/api/v1/counters_controller.rb
@@ -5,7 +5,11 @@ class Api::V1::CountersController < ApplicationController
       images: [{
         type: :share,
         social_network: [:facebook, :instagram, :twitter].sample,
-        url: Faker::Avatar.image,
+        url: [
+          "https://pbs.twimg.com/media/C8Bj3uZXUAAJIJy.jpg:large",
+          "https://pbs.twimg.com/media/C7JacAuWkAQLDCh.jpg:large",
+          "https://pbs.twimg.com/media/Cw6FR8dXgAAeFWf.jpg:large"
+        ].sample,
       }]
     }
   end
