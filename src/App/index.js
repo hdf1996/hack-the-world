7c0488dd029c2c2e8207e210cfd156730bee2235
Ariel Gerstein <ariel.ger@hotmail.com>
2017-04-08 12:14:36 -0300

    Add basic styles and move components to folders

--- src/App/index.js
+++ src/App/index.js
@@ -3,14 +3,14 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
-import BarView from './BarView';
+import BarView from '../BarView';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route path="/1" component={BarView} />
+           <Route path="/:barId" component={BarView}/>
         </div>
       </Router>
     );
---
+++ src/HashtagFeed/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+
+const HashtagFeed = () =>
+  <div className="hashtag-feed">
+    Hashtag Feed
+  </div>;
+
+export default HashtagFeed;
