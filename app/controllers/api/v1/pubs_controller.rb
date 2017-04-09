7cfd55c6c294191a1b331244a97ca47e0e439236
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-09 09:37:58 -0300

    Added order by goal

--- app/controllers/api/v1/pubs_controller.rb
+++ app/controllers/api/v1/pubs_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PubsController < ApplicationController
   def index
     @response = Pub.all
-    @response = @response.sort(&:goal_completed)
+    @response = @response.sort(&:goal_completed) if params[:order_by_goal].present?
     render json: @response
   end
 end
