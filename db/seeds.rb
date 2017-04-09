7f1101408403391a3c17c378c1bb59eaa2d9690a
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-08 21:00:51 -0300

    added things to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,34 @@
-[
-  {
-    name: "Distrito arcos"
+[{
+    name: "Distrito Arcos",
+    direction: "Paraguay 4979 - Centro Comercial Distrito",
+    telephone: "",
+    latitude: "-34.5811051",
+    longitude: "-58.4306089"
   },
   {
-    name: "Callao y Viamonte"
-  }, 
+    name: "Callao y Viamonte",
+    direction: "Callao 650",
+    telephone: "54 911 2091 3982",
+    latitude: "-34.6014216",
+    longitude: "-58.3927544"
+  },
   {
-    name: "Paraguay y Riobamba" 
+    name: "Paraguay y Riobamba",
+    direction: "Paraguay 1900",
+    telephone: "54 911 2091 9641",
+    latitude: "-34.5987254",
+    longitude: "-58.3950685"
   }
 ].each do |params|
-  Pub.first_or_create(params)
+  Pub.find_or_create_by(params).errors.inspect
 end
 
 {
-  HappyHourPatagoniaArcos: 1
+  HappyHourPatagoniaArcos: 1,
+  HappyHourPatagoniaCallao: 2,
+  HappyHourPatagoniaRiobamba: 3
 }.each do |key, value|
-  Hashtag.first_or_create(
+  Hashtag.find_or_create_by(
     name: key,
     pub: Pub.find(value),
     goal: 500
